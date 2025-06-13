697d38fb91e5170807a2423d49348596dde303ee
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-13 18:14:03 +0500

    Performed text processing

--- text-processing.py
+++ text-processing.py
@@ -5,3 +5,17 @@ from nltk.stem import WordNetLemmatizer
 
 nltk.download('stopwords')
 nltk.download('wordnet')
+
+def clean_text (text):
+    text = text.lower()
+    tokens = text.split()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return " ".join(tokens)
+
+data = pd.read_pickle("comments.pkl")
+
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words("english"))
+
+data["cleaned_comment"] = data["comment"].apply(clean_text)
+
