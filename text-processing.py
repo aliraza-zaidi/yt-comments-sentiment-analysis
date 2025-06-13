5b70848399ee73e1c7d7a0c2a771575bca3a71dd
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-13 22:24:05 +0500

    Converted to TextProcessor Class

--- text-processing.py
+++ text-processing.py
@@ -3,20 +3,28 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('stopwords')
-nltk.download('wordnet')
+class TextProcessor:
+    nltk.download('stopwords')
+    nltk.download('wordnet')
 
-def clean_text (text):
-    text = text.lower()
-    tokens = text.split()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
-    return " ".join(tokens)
+    def __init__ (self):        
+        self.lemmatizer = WordNetLemmatizer()
+        self.stop_words = set(stopwords.words("english"))
 
-data = pd.read_pickle("comments.pkl")
-
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words("english"))
-
-data["cleaned_comment"] = data["comment"].apply(clean_text)
-
-data.to_pickle("processed.pkl")
+    def load_data (self, filename):
+        self.data = pd.read_pickle(filename)
+        
+    def clean_text (self, text):
+        text = text.lower()
+        tokens = text.split()
+        tokens = [self.lemmatizer.lemmatize(word) for word in tokens if word not in self.stop_words]
+        return " ".join(tokens)
+    
+    def process_data (self):
+        self.data["cleaned_comment"] = self.data["comment"].apply(self.clean_text)
+        
+    def process_comment (self, comment):
+        return self.clean_text(comment)
+    
+    def save_data (self):
+        self.data.to_pickle('processed.pkl')
