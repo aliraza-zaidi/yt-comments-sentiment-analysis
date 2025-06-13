d5361e09dcb8fa4ecc8771113afb135fbc9c31ac
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-14 00:24:16 +0500

    Implemented comments predictor

--- fetch_comments.py
+++ fetch_comments.py
@@ -2,6 +2,7 @@ import re
 import emoji
 from langdetect import detect
 from googleapiclient.discovery import build
+import joblib
 
 def clean_comment (comment):
     comment = emoji.replace_emoji(comment, replace="")
@@ -40,3 +41,14 @@ def fetch_comments(video_id, api_key, max_comments=100, max_len=150):
             break
 
     return comments
+
+model = joblib.load('sentiment_model.pkl')
+
+def predict_comments(comments, model):
+    predictions = []    
+    
+    for comment in comments:
+        prediction = model.predict([comment])[0]
+        predictions.append("Positive" if prediction == 1 else "Negative")
+
+    return list(zip(comments, predictions))
