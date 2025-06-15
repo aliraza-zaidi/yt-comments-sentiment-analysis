7128448ed74bc920ba3f4b49de48d7cb3cedef28
aliraza-zaidi <alirazaidi.official@gmail.com>
2025-06-15 17:35:38 +0500

    Moved files around

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from fetch_comments import fetch_comments
-from text_processing import TextProcessor
-from analyze import predict_comments
+from res.fetch_comments import fetch_comments
+from res.text_processing import TextProcessor
+from res.analyze import predict_comments
 import joblib
 import numpy as np
 from collections import Counter
